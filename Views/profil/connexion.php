d6c2d62d251173a35f42f227c41d931fa2b7844a
Boris <boris.tikhomiroff@lapleforme.io>
2022-04-04 09:13:08 +0200

    layout formulaire terminé

--- Views/profil/connexion.php
+++ Views/profil/connexion.php
@@ -1,5 +1,5 @@
 <article class="form">
-    <h1 class="form_title">Connexion</h1>
+    <h1 class="form__title">Connexion</h1>
 
     <?php if (isset($_SESSION['flash'])) : ?>
         <?php foreach ($_SESSION['flash'] as $type => $message) : ?>
@@ -11,15 +11,20 @@
         <?php unset($_SESSION['flash']) ?>
     <?php endif; ?>
 
-    <form class="form" action="#" method="post">
+    <form class="form__container" action="#" method="post">
+        <div class="form__field">
+            <label for="email">Email : </label>
+            <input class="form__text" type="text" id="email" name="email">
+        </div>
+        <div class="form__field">
+            <label for="mdp">Mot de passe : </label>
+            <input class="form__text" type="password" id="mdp" name="mdp">
+        </div>
 
-        <label for="email">Email : </label>
-        <input class="form__text" type="text" id="email" name="email">
+        <div class="form__field">
+            <input class="form__button" type="submit" name="submit" value="Connexion">
+        </div>
 
-        <label for="mdp">Mot de passe : </label>
-        <input class="form__text" type="password" id="mdp" name="mdp">
-
-        <input class="form__button" type="submit" name="submit" value="Connexion">
     </form>
     <a href="checkemail">Mot de passe oublié ?</a>
     <a class="form__link" href="inscription">Je créer un compte</a>
--- Views/profil/inscription.php
+++ Views/profil/inscription.php
@@ -3,55 +3,60 @@
     <section class="alert">
 
         <?php if (isset($_SESSION['flash'])) : ?>
-            
+
             <?php foreach ($_SESSION['flash'] as $type => $message) : ?>
-                
+
                 <p class="alert__message"><?= $message; ?></p>
             <?php endforeach; ?>
         <?php endif; ?>
 
         <?php if (isset($_SESSION['flash'])) :  ?>
-            
+
             <?php unset($_SESSION['flash']) ?>
         <?php endif; ?>
     </section>
-    
-    
+
+
     <form class="form" action="" method="post">
 
         <h1 class="form__title">Inscription</h1>
-        <div class="form__field">
 
+        <div class="form__field">
             <label class="form__label" for="prenom">Prenom :</label>
             <input class="form__text" type="text" id="prenom" name="prenom" aria-required="true">
         </div>
 
         <div class="form__field">
+            <label for="question" class="form__label">Ecrire votre question secrete :</label>
+            <input type="text" id="question" name="question_secrete" aria-required="true" class="form__text">
+        </div>
 
-        <label for="email">Ecrire votre question secrete :</label>
-        <input type="text" id="email" name="question_secrete" aria-required="true">
+        <div class="form__field">
+            <label for="reponse" class="form__label">Reponse :</label>
+            <input type="password" id="reponse" name="reponse" aria-required="true" class="form__text">
+        </div>
 
-        <label for="email">reponse :</label>
-        <input type="password" id="email" name="reponse" aria-required="true">
+        <div class="form__field">
+            <label for="mdp" class="form__label">Mot de passe : </label>
+            <input type="password" id="mdp" name="mdp" aria-required="true" class="form__text">
+        </div>
 
-        <label for="mdp">Mot de passe : </label>
-        <input type="password" id="mdp" name="mdp" aria-required="true">
+        <div class="form__field">
             <label class="form__label" for="nom">Nom :</label>
             <input class="form__text" type="text" id="nom" name="nom" aria-required="true">
         </div>
 
         <div class="form__field">
-
             <label class="form__label" for="email">Email :</label>
             <input class="form__text" type="text" id="email" name="email" aria-required="true">
         </div>
-        
+
         <div class="form__field">
 
             <label class="form__label" for="mdp">Mot de passe : </label>
             <input class="form__text" type="password" id="mdp" name="mdp" aria-required="true">
         </div>
-        
+
         <div class="form__field">
 
             <label class="form__label" for="mdpConfirm">Confirmez votre mot de Passe :</label>
@@ -59,7 +64,7 @@
         </div>
 
         <div class="form__field">
-            
+
             <input class="form__button" type="submit" name="submit" value="Inscription">
         </div>
         <a class="form__link" href="connexion">J'ai déjà un compte ></a>

c9fe2e7445378918c07ba4af98921a66c518ec3d
Johan <johan.bouguermouh@laplateforme.io>
2022-04-08 09:55:17 +0200

    Mise en page des tableau plus formulaire

--- Views/administrator/updateSku.php
+++ Views/administrator/updateSku.php
@@ -1,37 +1,97 @@
-<h3>Gestion de Stock</h3>
-<section>
-    <h1>Admin</h1>
-    <ul>
-        <li><a href="creerArticle/partie1">Creer un Article ></a></li>
-        <li><a href="modifierArticle/liste">Modifier un articles ></a></li>
-        <li><a href="../gestiondestock">Gestion des stocks ></a></li>
-        <li><a href="">Historique de commande ></a></li>
-        <li><a href="">Gestion de livraison ></a></li>
-        <li><a href="gestionUtilisateur/liste">Gestion des utilisateurs</a></li>
-        <li><a href="../profil/deconnexion">Se deconnecter</a></li>
-    </ul>
-</section>
-<article>
-    <h4>Produit au stock limités</h4>
+<article class="mainAndSideAdmin">
+    <section class="sideBarreAcount">
+        <div>
+            <h1>Admin</h1>
+            <ul>
+                <li><a href="creerArticle/partie1">Creer un Article</a><i class="fa-solid fa-angle-right"></i></li>
+                <li><a href="modifierArticle/liste">Modifier un articles</a><i class="fa-solid fa-angle-right"></i></li>
+                <li><a href="../gestiondestock">Gestion des stocks</a><i class="fa-solid fa-angle-right"></i></li>
+                <li><a href="">Historique de commande</a><i class="fa-solid fa-angle-right"></i></li>
+                <li><a href="">Gestion de livraison</a><i class="fa-solid fa-angle-right"></i></li>
+                <li><a href="gestionUtilisateur/liste">Gestion des utilisateurs</a><i class="fa-solid fa-angle-right"></i></li>
+                <li><a href="../profil/deconnexion">Se deconnecter</a></li>
+            </ul>
+        </div>
+    </section>
+    <article class="mainUpdateProduct">
+    <h1>Gestion de Stock</h1>
+    <article class="tableEmergencyRestock">
+        <h2>Produit au stock limités</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nom de l'Article</th>
+                    <th>Catégorie Principale</th>
+                    <th>SKU</th>
+                    <th>selectionner</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($urgentStock as $value) : ?>
+                    <tr id="<?= $value['id_article'] ?>">
+                        <td><?= $value['titre_article'] ?></td>
+                        <td><?= $value['cat parent'] ?></td>
+                        <td><?= $value['sku'] ?></td>
+                        <td>
+                            <form action="#<?= $value['id_article'] ?>" method="POST">
+                                <?php if (empty($_POST['enterFormNow' . $value['id_article']])) : ?>
+                                    <button type="submit" name="enterFormNow<?= $value['id_article'] ?>">Restocker</button>
+                                <?php else : ?>
+                                    <input type="text" name="sku" value="<?= $value['sku'] ?>">
+                                    <button type="submit" name="id_article" value="<?= $value['id_article'] ?>">Ajouter au Stock</button>
+                                <?php endif; ?>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </article>
+    <section class="formSearchUpdateProduct">
+        <form action="" method="GET">
+            <fieldset>
+                <legend>Recherche avancée</legend>
+                <label for="site-search">Rechercher par nom</label>
+                <input type="search" name="recherche" aria-label="Search through site content">
+                <button>Search</button>
+            </fieldset>
+        </form>
+        <form action="<?= $urlRedirect ?>" method="POST">
+            <label for="PRINCIPALE ">Trier par Catégories Principales</label>
+            <select list="list_categories" name="PRINCIPALE" id="list_categories">
+                <datalist id="list_categories">
+                    <option></option>
+                    <?php foreach ($allCategories as $value) : ?>
+                        <option value="<?= $value['nom_categorie'] ?>"><?= $value['nom_categorie'] ?>(<?= $methodImport->countSearch($value['nom_categorie']) ?>)</option>
+                    <?php endforeach; ?>
+                </datalist>
+            </select>
+            <input type="submit" value='appliquer'>
+        </form>
+    </section>
+
     <table>
+        <h2>Tout les articles</h2>
         <thead>
-            <tr>Nom de l'Article</tr>
-            <tr>Catégorie Principale</tr>
-            <tr>Nombre unitaire en stock</tr>
-            <tr>selectionner</tr>
+            <tr>
+                <th>Nom de l'Article</th>
+                <th>Catégorie Principale</th>
+                <th>SKU</th>
+                <th>selectionner</th>
+            </tr>
         </thead>
         <tbody>
-            <?php foreach ($urgentStock as $value) : ?>
+            <?php foreach ($resultSearch as $value) : ?>
                 <tr id="<?= $value['id_article'] ?>">
                     <td><?= $value['titre_article'] ?></td>
                     <td><?= $value['cat parent'] ?></td>
                     <td><?= $value['sku'] ?></td>
                     <td>
                         <form action="#<?= $value['id_article'] ?>" method="POST">
-                            <?php if (empty($_POST['enterFormNow' . $value['id_article']])) : ?>
-                                <button type="submit" name="enterFormNow<?= $value['id_article'] ?>">Restocker</button>
+                            <?php if (empty($_POST['enterForm' . $value['id_article']])) : ?>
+                                <button type="submit" name="enterForm<?= $value['id_article'] ?>" value="<?= $value['id_article'] ?>">Restocker</button>
                             <?php else : ?>
-                                <input type="text" name="sku" value="<?= $value['sku'] ?>">
+                                <input type="text" name="sku">
                                 <button type="submit" name="id_article" value="<?= $value['id_article'] ?>">Ajouter au Stock</button>
                             <?php endif; ?>
                         </form>
@@ -40,53 +100,5 @@
             <?php endforeach; ?>
         </tbody>
     </table>
+    </article>
 </article>
-<form action="" method="GET">
-    <fieldset>
-        <legend>Recherche avancée</legend>
-        <label for="site-search">Rechercher par nom</label>
-        <input type="search" name="recherche" aria-label="Search through site content">
-        <button>Search</button>
-    </fieldset>
-</form>
-<form action="<?= $urlRedirect ?>" method="POST">
-    <label for="PRINCIPALE ">Trier par Catégories Principales</label>
-    <select list="list_categories" name="PRINCIPALE" id="list_categories">
-        <datalist id="list_categories">
-            <option></option>
-            <?php foreach ($allCategories as $value) : ?>
-                <option value="<?= $value['nom_categorie'] ?>"><?= $value['nom_categorie'] ?>(<?= $methodImport->countSearch($value['nom_categorie']) ?>)</option>
-            <?php endforeach; ?>
-        </datalist>
-    </select>
-    <input type="submit" value='appliquer'>
-</form>
-
-<table>
-    <h4>Tout les articles</h4>
-    <thead>
-        <tr>Nom de l'Article</tr>
-        <tr>Catégorie Principale</tr>
-        <tr>Nombre unitaire en stock</tr>
-        <tr>selectionner</tr>
-    </thead>
-    <tbody>
-        <?php foreach ($resultSearch as $value) : ?>
-            <tr id="<?= $value['id_article'] ?>">
-                <td><?= $value['titre_article'] ?></td>
-                <td><?= $value['cat parent'] ?></td>
-                <td><?= $value['sku'] ?></td>
-                <td>
-                    <form action="#<?= $value['id_article'] ?>" method="POST">
-                        <?php if (empty($_POST['enterForm' . $value['id_article']])) : ?>
-                            <button type="submit" name="enterForm<?= $value['id_article'] ?>" value="<?= $value['id_article'] ?>">Restocker</button>
-                        <?php else : ?>
-                            <input type="text" name="sku">
-                            <button type="submit" name="id_article" value="<?= $value['id_article'] ?>">Ajouter au Stock</button>
-                        <?php endif; ?>
-                    </form>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>

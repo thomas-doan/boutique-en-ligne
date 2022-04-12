b7898a31b5d70dd0e9ecf199cd873545c9898b90
Boris <boris.tikhomiroff@lapleforme.io>
2022-04-12 18:26:41 +0200

    Auto stash before merge of "main" and "origin/main"

--- Views/profil/commande.php
+++ Views/profil/commande.php
@@ -11,7 +11,8 @@
             </ul>
         </div>
     </section>
-    <article class="order">
+
+    <!-- <article class="order">
         <div class="order__resume">
             <?php foreach ($order_resume as $key => $order) { ?>
 
@@ -45,5 +46,47 @@
             <p><span>Prix total :</span><?= $order['prix_avec_tva'] ?> € ttc</p>
         </section>
         </div>
-    </article>
+    </article> -->
+
+        <div class="containerMain orderHistory">
+        <div class="layoutContainertable orderHistory">
+
+            <div>
+                <article>
+                <?php foreach ($order_resume as $key => $order) { ?>
+
+<section>
+    <h1 class="order__title">Commande N°<?= $order['fk_id_num_commande'] ?></h1>
+    <p><span>Livrée le : </span><?= $order['date'] ?></p>
+
+    <p><span>Adresse de livraison : </span></p>
+
+    <p><?= $order['voie'] ?></p>
+    <p><?= $order['voie_sup'] ?></p>
+    <p><?= $order['code_postal'] . ' ' . $order['ville'] ?></p>
+</section>
+<?php break;
+} ?>
+
+<section>
+                <?php foreach ($order_resume as $key => $order) { ?>
+                    <h2 class="order__title">Article <?= $key + 1 ?></h2>
+                    <p><span>Article: </span><?= $order['titre_article'] ?></p>
+                    <p> <span>Quantité: </span><?= $order['nb_article'] ?></p>
+                    <p><span>Prix: </span><?= $order['prix_article'] ?> € ttc</p>
+                    <?php } ?>
+                </section>
+
+    
+                    
+        <section>
+            <h2 class="order__title">Prix total</h2>
+            <p><span>Total d'articles : </span><?= $order['total_produit'] ?> </p>
+            <p><span>Prix total :</span><?= $order['prix_avec_tva'] ?> € ttc</p>
+        </section>
+                </article>
+            </div>
+        </div>
+    </div>
+
 </article>
